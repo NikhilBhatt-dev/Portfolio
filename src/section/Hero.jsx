d9b536cab713f6cecb2fde3677f9fe3ee1c8b7a3
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-17 20:40:41 +0530

    add 3D Components

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import {Canvas} from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
+import HackerRoom from '../components/HackerRoom';
+import CanvasLoder from '../components/CanvasLoder';
+
 
 const Hero = () => {
   return (
-    <section className='min-h-screen border-8 border-blue-100'></section>
+    <section className='min-h-screen w-full flex flex-col relative'>
+      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Nikhil Bhatt
+          <span className='waving-hand'>👋</span>
+        </p>
+
+        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+      </div>
+
+      <div className='w-full h-full absolute inset-0'>
+
+        <Canvas className='w-full h-full'>
+          <Suspense fallback={<CanvasLoder />}>
+         
+          <PerspectiveCamera makeDefault position={[0,0,30]}  />
+
+            <HackerRoom scale={0.07} position={[0,0,0]}  roation={[0,0,0]} />
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10,10,10]} intensity={0.5} /> 
+            </Suspense>
+
+        </Canvas>
+
+      </div>
+    </section>
   )
 }
 
 export default Hero
+
+//2.03
