71d9c866f0cd68cc14bfac12497d50c44b055473
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-20 20:20:55 +0530

    add filetree

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './section/Nav'
 import Hero from './section/Hero'
+import About from './section/About'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <Nav />
       <Hero />
+      <About />
     </main>
   )
 }
