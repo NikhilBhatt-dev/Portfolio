61bf9d65e032d1beb5abe8358d4d6b21ed12362e
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-21 12:08:57 +0530

    add contact

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Globe from 'react-globe.gl'
+import Button from '../components/Button'
 
 const About = () => {
   return (
     <section className='c-space my-20'>
-      <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
 
         <div className='col-span-1 xl:row-span-3'>
           <div className='grid-container'>
-            <img 
-              src="/assets/grid1.png" 
-              alt="grid-1" 
+            <img
+              src="/assets/grid1.png"
+              alt="grid-1"
               className='w-full sm:h-[276px] h-fit object-contain'
             />
 
@@ -30,9 +31,9 @@ const About = () => {
 
         <div className='col-span-1 xl:row-span-3'>
           <div className="grid-container">
-            <img 
-              src="/assets/grid2.png" 
-              alt="grid2" 
+            <img
+              src="/assets/grid2.png"
+              alt="grid2"
               className='w-full sm:w-[276px] h-fit object-contain'
             />
 
@@ -46,37 +47,33 @@ const About = () => {
         </div>
 
 
-        {/* <div className='col-span-1 xl:row-span-4'>
-            <div className='grid-container'>
-                <div  className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-                    <Globe 
-                    height={326}
-                    width={326}
-                    backgroundColor='rgba(0,0,0,0)'
-                    backgroudImageOpacity={0.5}
-                     showAtmosphere
-                     showGraticules
-                     globeimageUrl={}/>
-
-                </div>
-            </div>
-        </div> */}
-        <div className='col-span-1 xl:row-span-4'>
-  <div className='grid-container'>
-    <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-      <Globe
-        height={326}
-        width={326}
-        backgroundColor="rgba(0,0,0,0)"
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-        showAtmosphere={true}
-        showGraticules={true}
         
-      />
-    </div>
-  </div>
-</div>
+        <div className='col-span-1 xl:row-span-3'>
+          <div className='grid-container'>
+            <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                showAtmosphere={true}
+                showGraticules={true}
+
+              />
+            </div>
+            <div>
+              <p className='grid-headtext'>
+                I work remotely across most timezones.
+              </p>
+              <p className='grid-subtext'>
+                I'm based in India, with remote work available
+              </p>
+              <Button name='Contact Me' isBeam containerClass='w-full mt-10' />
+            </div>
+
+          </div>
+        </div>
 
       </div>
     </section>
