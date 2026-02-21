b6dfd3a9f07ee8136db36d39fad367074a2580a9
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-02-21 19:25:25 +0530

    add model

--- src/section/Projects.jsx
+++ src/section/Projects.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import { myProjects } from '../constants'
+import { Canvas } from `@react-three/fiber`
+import { Center, OrbitControls, Sphere } from '@react-three/drei'
+import DemoComputer from '../components/DemoComputer';
 
 const projectsCount = myProjects.length;
 
@@ -96,6 +99,32 @@ const Projects = () => {
             </button>
           </div>
 
+        </div>
+
+
+
+
+
+        <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full'>
+
+          <Canvas>
+            <ambientLight intensity={Math.PI / 2} />
+            <directionalLight position={[10, 10, 5]} />
+            <Center>
+
+              <Suspense fallback={<CanvasLoder />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+
+                  <DemoComputer />
+
+
+                </group>
+              </Suspense>
+
+            </Center>
+            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+
+          </Canvas>
         </div>
 
       </div>
